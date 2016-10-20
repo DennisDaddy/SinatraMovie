e6135db09866334a1a6e78064093e95067118c6d
DennisDaddy <softteez95@gmail.com>
2016-10-20 09:13:19 +0300

    assign values to attribute of the movie

--- app.rb
+++ app.rb
@@ -20,5 +20,8 @@ get('/movies/new') do
 end
 
 post('/movies/create') do
-	"Received: #{params.inspect} "
+	@movie = Movie.new
+	@movie.title = params['title']
+	@movie.director = params['director']
+	@movie.year = params['year']
 end
